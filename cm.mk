59fd685a3dd8f1a5cc843722f6def5d932e2dbfc
Adam Farden <adam@farden.cz>
2013-03-13 00:04:29 +0100

    Update to JB Build strings

--- cm.mk
+++ cm.mk
@@ -6,7 +6,7 @@ $(call inherit-product, vendor/cm/config/gsm.mk)
 # Inherit CM common Phone stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=ST26i_1257-4009 BUILD_FINGERPRINT=SEMC/ST26i_1257-4009/ST26i:4.0.4/6.1.A.0.452/O_5_zw:user/release-keys PRIVATE_BUILD_DESC="ST26i-user 4.0.4 6.1.A.0.452 O_5_zw test-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=ST26i_1270-7015 BUILD_FINGERPRINT=Sony/ST26i_1270-7015/ST26i:4.1.2/11.2.A.0.21/47_96g:user/release-keys PRIVATE_BUILD_DESC="ST26i-user 4.1.2 2.10.J.0.28 47_96g test-keys"
 
 TARGET_SCREEN_HEIGHT := 854
 TARGET_SCREEN_WIDTH := 480
